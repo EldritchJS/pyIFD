bd068f3bd3d392e05545d28c4e0ff9ec74ac9248
grantjensen <gjensen6@wisc.edu>
2021-09-01 19:43:48 -0500

    Sped up GHOST. Combined the fftconvolve so that it is performed less. Also switched imread/imwrite to imencode.

--- src/pyIFD/GHOST.py
+++ src/pyIFD/GHOST.py
@@ -15,7 +15,6 @@ from scipy.signal import fftconvolve
 from skimage.transform import resize
 import numpy as np
 import cv2
-import os
 
 
 def GHOST(impath, checkDisplacements=0):
@@ -51,33 +50,23 @@ def GHOST(impath, checkDisplacements=0):
         maxDisp = 0
 
     smoothing_b = 17
+    h = (np.ones((smoothing_b, smoothing_b))/(smoothing_b**2))[:, :, None]
     Offset = int((smoothing_b-1)/2)
 
     for ii in range(minQ, maxQ+1, stepQ):
-        cv2.imwrite('tmpResave.jpg', imorig, [cv2.IMWRITE_JPEG_QUALITY, ii])
-        tmpResave = np.double(cv2.imread('tmpResave.jpg'))
+        encimg = cv2.imencode('.jpg', imorig, [int(cv2.IMWRITE_JPEG_QUALITY), ii])[1].tobytes()
+        nparr = np.frombuffer(encimg, np.byte)
+        tmpResave = np.double(cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR))
         Deltas = {}
         overallDelta = {}
+        ComparisonFull = (tmpResave-imorig)**2
+        ComparisonFull = np.double(fftconvolve(ComparisonFull, h, 'same'))
         for dispx in range(maxDisp+1):
             for dispy in range(maxDisp+1):
                 DisplacementIndex = dispx*8+dispy
-                tmpResave_disp = tmpResave[dispx:, dispy:, :]
-                if(dispx == 0 and dispy == 0):  # This if/elif statement removes the last dispx entries of the x-coord, and the last dispy entries of the y-coord
-                    imorig_disp = imorig
-                elif(dispx == 0):
-                    imorig_disp = imorig[:, 0:-dispy, :]
-                elif(dispy == 0):
-                    imorig_disp = imorig[0:-dispx, :, :]
-                else:
-                    imorig_disp = imorig[0:-dispx, 0:-dispy, :]
-                Comparison = (imorig_disp-tmpResave_disp)**2
-                h = np.ones((smoothing_b, smoothing_b))/(smoothing_b**2)
-                for jj in range(3):
-                    Comparison[:, :, jj] = fftconvolve(Comparison[:, :, jj], h, mode='same')  # 2d convolution
-                Comparison = Comparison[Offset:-Offset, Offset:-Offset, :]
+                Comparison=ComparisonFull[Offset+dispx:-Offset, Offset+dispy:-Offset]
                 Deltas[DisplacementIndex] = np.mean(Comparison, axis=2)
                 overallDelta[DisplacementIndex] = np.mean(Deltas[DisplacementIndex])
-
         minInd = min(overallDelta, key=overallDelta.get)
         minOverallDelta = overallDelta[minInd]
         Mins[int((ii-minQ)/stepQ)] = minInd+1  # Add 1 to acct for matlab starting idx counting at 1
@@ -86,7 +75,6 @@ def GHOST(impath, checkDisplacements=0):
         delta = (delta-delta.min())/(delta.max()-delta.min())
         newSize = (round((delta.shape[i])/4) for i in range(2))
         dispImages[int((ii-minQ)/stepQ)] = resize(np.float32(delta), newSize)
-
     OutputX = range(minQ, maxQ+1, stepQ)
     OutputY = Output
     imin = signal.argrelextrema(OutputY, np.less)[0]+1  # Add one to acct for matlab starting idx counting at 1.
@@ -95,5 +83,4 @@ def GHOST(impath, checkDisplacements=0):
     if(OutputY[0] < OutputY[1]):  # Check first point
         imin = np.insert(imin, 0, 1)
     Qualities = imin*stepQ+minQ-1
-    os.remove("tmpResave.jpg")
-    return [OutputX, OutputY, dispImages, imin, Qualities, Mins]
+    return [OutputX, OutputY, dispImages, imin, Qualities, Mins]    
