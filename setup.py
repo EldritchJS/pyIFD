b4d37bc010871836204db1a2d5edc78c1c0b4bce
EldritchJS <jschlessman@gmail.com>
2021-10-13 15:08:24 -0400

    Added pypi dependency on jpegio

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyIFD',
-    version='0.0.1',
+    version='0.0.2',
     extras_require=dict(tests=['pytest']),
     packages=find_packages(where="src"),
     package_dir={"": "src"},
@@ -17,5 +17,6 @@ setup(
         'scikit-image',
         'pillow',
         'opencv-python',
-        'PyWavelets',]
+        'PyWavelets',
+        'jpegio']
 )
